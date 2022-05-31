767a88d513dc4f8e1c1b13de714966d4f5e1472a
zerabruck <zerabruckm@gmail.com>
2022-05-31 18:27:55 +0300

    login added

---
+++ gojo_flutter/lib/login_and_post/login.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  final _formKey = GlobalKey<FormState>();
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.pink;
+      }
+      return Colors.blue;
+    }
+
+    return Scaffold(
+      body: Container(
+        child: ListView(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Image(
+                image: AssetImage('assets/food_6.jpeg'),
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Text(
+                        "Sign in to your account",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ]),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 15),
+                        child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Email"),
+                              Icon(
+                                Icons.star_rate_rounded,
+                                size: 6,
+                                color: Colors.red[400],
+                              )
+                            ])),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: "enter your email please",
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                          )),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "please enter your email";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 15),
+                        child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("password"),
+                              Icon(
+                                Icons.star_rate_rounded,
+                                size: 6,
+                                color: Colors.red[400],
+                              )
+                            ])),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          suffixIcon: Padding(
+                            padding:
+                                const EdgeInsetsDirectional.only(end: 12.0),
+                            child: Icon(Icons
+                                .remove_red_eye), // myIcon is a 48px-wide widget.
+                          ),
+                          labelText: "enter your pasword",
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                          )),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "please enter your password";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Checkbox(
+                          checkColor: Colors.white,
+                          fillColor:
+                              MaterialStateProperty.resolveWith(getColor),
+                          value: isChecked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              isChecked = value!;
+                            });
+                          },
+                        ),
+                        Text(
+                          'Remeber me',
+                          style: TextStyle(color: Colors.black54),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 20),
+                        ),
+                        onPressed: () {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            // If the form is valid, display a snackbar. In the real world,
+                            // you'd often call a server or save the information in a database.
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
+                            );
+                          }
+                        },
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Column(
+                                children: [
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text('Sign in'),
+                                  SizedBox(
+                                    height: 10,
+                                  )
+                                ],
+                              )
+                            ]),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Center(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 15),
+                        ),
+                        onPressed: () {},
+                        child: const Text('forgot password?'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 65,
+                    ),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account?",
+                            style:
+                                TextStyle(color: Colors.black54, fontSize: 12),
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: const TextStyle(fontSize: 12),
+                            ),
+                            onPressed: () {},
+                            child: const Text('sign up'),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
